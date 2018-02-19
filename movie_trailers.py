4b5411c5e541b63aad1c4c3d4d2de857d0dffb13
myasul <yasulmatthew@gmail.com>
2018-02-19 17:59:37 +0800

    Changes made:     - Add functionality to add/delete movies in a JSON file.     - Add JSON file and template     - Add few poster images

--- movie_trailers.py
+++ movie_trailers.py
@@ -1,29 +1,32 @@
 import media
 import fresh_tomatoes
+import json
 
 
-imitation_game = media.Movie(
-    "The Imitation Game",
-    "Life and works of Alan Turing",
-    "file:///Users/matthewyasul/Documents/Programming Documents/FS_Nanodegree/Projects/movie_trailer/posters/the_imitation_game.jpg",
-    "https://www.youtube.com/watch?v=S5CjKEFb-sM"
-)
+def retrieve_movies(filename):
 
+    # path that contains the poster images
+    poster_path = "file:///Users/matthewyasul/Documents/Programming Documents/" \
+        "FS_Nanodegree/Projects/movie_trailer/posters/{poster_name}"
 
-"""inception = media.Movie(
-    "Inception",
-    "A man who expose secrets by travelling into their dreams.",
-    "https://pre00.deviantart.net/ab77/th/pre/i/2011/188/4/9/inception_poster_by_adamrabalais-d3lcwkz.jpg",
-    "https://www.youtube.com/watch?v=YoHD9XEInc0"
-)"""
+    #loads the data from the json file and transfer it to a dictionary variable
+    with open(filename, 'r') as f:
+        movies = json.loads(f.read())
 
-wolf_of_ws = media.Movie(
-    "The Wolf of Wall Street",
-    "Based on the true story of Jordan Belfort",
-    "http://1.bp.blogspot.com/-JrZ2BuYcpBc/UndddeSAhPI/AAAAAAAATtU/aU3mDsy3Izk/s0/The_Wolf_of_Wall_Street-poster-3.jpg",
-    "https://www.youtube.com/watch?v=iszwuX1AK6A"
-)
+    movie_list = []
 
-movies = [imitation_game, wolf_of_ws]
+    for movie in movies:
+        #update value of poster to include absolute path
+        movie["poster"] = poster_path.format(poster_name=movie["poster"])
+        #creates a movie instance
+        new_movie_entry = media.Movie(
+            movie["title"],
+            movie["storyline"],
+            movie["poster"],
+            movie["trailer"])
+        movie_list.append(new_movie_entry)
 
-fresh_tomatoes.open_movies_page(movies)
+    return movie_list
+
+#passes a list of movie instances to display in an html page
+fresh_tomatoes.open_movies_page(retrieve_movies('movie.json'))
